9f863155edf12adb80c623ff2b4dee8bfad2eb35
Dio <diotrifirnanda43@gmail.com>
2026-07-06 18:00:00 +0700

    feat: constants + connectivity service

---
+++ lib/shared/constants/api_constants.dart
@@ -0,0 +1,7 @@
+class ApiConstants {
+  ApiConstants._();
+
+  static const String baseUrl = 'http://10.0.2.2:8000/api/v1';
+  static const Duration timeout = Duration(seconds: 30);
+  static const Duration syncInterval = Duration(seconds: 30);
+}
